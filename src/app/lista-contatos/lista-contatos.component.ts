61f9e48b52cb0068ea9190de08fa4eecb4158457
Diego <diegoteixeirapereira@hotmail.com>
2020-12-04 14:32:01 -0300

    criação do arquivo lista-contatos

---
+++ src/app/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Contato } from '../models/contato.model';
+import { ContatoService } from '../services/contato.service';
+
+@Component({
+  selector: 'app-lista-contatos',
+  templateUrl: './lista-contatos.component.html',
+  styleUrls: ['./lista-contatos.component.css']
+})
+export class ListaContatosComponent implements OnInit {
+
+  contatos: Contato[] = [];
+
+  constructor(private _contatoService: ContatoService,) { }
+
+  ngOnInit() {
+    this._contatoService.retornarTodos().subscribe(
+      c => this.contatos = c,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+  get filtrarContatos() {
+    return this.contatos.filter( x => x.id > 0);
+  }
+
+}
