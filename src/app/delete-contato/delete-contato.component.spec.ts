f70efab8e0302ffe3303a6766f56490e793686b6
Diego <diegoteixeirapereira@hotmail.com>
2020-12-04 14:33:41 -0300

    criação do arquivo delete-contato

---
+++ src/app/delete-contato/delete-contato.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeleteContatoComponent } from './delete-contato.component';
+
+describe('DeleteContatoComponent', () => {
+  let component: DeleteContatoComponent;
+  let fixture: ComponentFixture<DeleteContatoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeleteContatoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeleteContatoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
