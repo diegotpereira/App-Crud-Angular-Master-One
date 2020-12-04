96ff131859ec0e3d994411c2d5c3f753dcbd2055
Diego <diegoteixeirapereira@hotmail.com>
2020-12-04 14:24:27 -0300

    criação do arquivo contato.model.ts

---
+++ src/app/Models/contato.model.ts
@@ -0,0 +1,11 @@
+import { calcPossibleSecurityContexts } from "@angular/compiler/src/template_parser/binding_parser";
+
+export class Contato {
+
+    id?: number;
+    nome?: string;
+    email?: string;
+    telefone?: string;
+}
+
+
